01ed27769de76df7acb86d22250c28861dee0d6c
Prolaser1 <benworsley05@gmail.com>
2023-03-29 11:16:51 +0100

    Added files

---
+++ constants.py
@@ -0,0 +1,6 @@
+#Change this to the location of your Chrome profile 
+profile = "/Users/benworsley/Library/Application Support/Google/Chrome/Profile 1"
+#Change this to the streamer's chat you want to bot
+streamer_chat = 'https://twitch.tv/synn/chat'
+#Add triggers that you want the bot to respond to in the chat
+raffle_triggers = ["!raffle", "!sraffle"]
